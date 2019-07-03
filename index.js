b393688d2dfa3651d9e43a1f8125ea88824fbde4
Pradip Hamal <pradip.hamal31@gmail.com>
2019-07-03 23:01:09 +0545

    Added schema for customer

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 var mongoose = require('mongoose');
 
 var mongo = mongoose.createConnection('mongodb://localhost/cibt_project', { useNewUrlParser: true });
-console.log(mongo);
+
+var Schema = mongoose.Schema;
+
+var customerSchema = new Schema({
+    _id: mongoose.Types.ObjectId(),
+    firstName: String,
+    lastName: String,
+    email: String,
+    contactNo: String,
+    createdDate: Date,
+    modifiedDate: Date,
+    status: Boolean
+});
