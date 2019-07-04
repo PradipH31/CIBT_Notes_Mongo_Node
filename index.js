424fbeae1468f4d37fd8825b8ccf8c80c677ff0d
Pradip Hamal <pradip.hamal31@gmail.com>
2019-07-04 11:58:09 +0545

    Used updateMany

--- index.js
+++ index.js
@@ -17,6 +17,11 @@ var customerSchema = new Schema({
 
 var Customer = mongo.model('Customer', customerSchema, "customers");
 
-Customer.findById('5d1ce7a4e04c5a3c3836123e', (err, row) => {
-    console.log(row);
+Customer.updateMany({ status: false }, {
+    $set: {
+        status: true,
+        modifiedDate: new Date()
+    }
+}, function(err, raw) {
+    console.log(raw);
 });
