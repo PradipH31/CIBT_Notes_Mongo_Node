88583ce073120c7917d4fcb317d523e3c6e201fb
Pradip Hamal <pradip.hamal31@gmail.com>
2019-07-04 12:29:20 +0545

    Added node, express & ejs

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ var mongoose = require('mongoose');
 
 var Customer = require('./models/customer')
 
-var customer = new Customer({
-    _id: mongoose.Types.ObjectId(),
-    firstName: 'Manjit',
-    lastName: 'Shakya',
-    email: 'mnzit@gmail.com',
-    contactNo: '8392457893',
-    createdDate: new Date(),
-    status: false
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    Customer.find({}, function(err, rows) {
+        res.render('customers/index', {
+            customers: rows
+        })
+    });
 });
 
-customer.save(function(err) {
-    if (err) {
-        console.log('er');
-    }
-    console.log('Saved')
-});
+app.listen(9000, () => {
+    console.log("Server running at 9000");
+})
