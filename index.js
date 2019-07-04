52f0b07ee2102a68228d66e722cab76e37741543
Pradip Hamal <pradip.hamal31@gmail.com>
2019-07-04 12:45:04 +0545

    Implemented add in node

--- index.js
+++ index.js
@@ -4,14 +4,35 @@ var Customer = require('./models/customer')
 
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
     Customer.find({}, function(err, rows) {
         res.render('customers/index', {
             customers: rows
         })
+    });
+});
+
+app.post('/', function(req, res) {
+    var customer = new Customer({
+        _id: mongoose.Types.ObjectId(),
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        contactNo: req.body.contactNo,
+        createdDate: new Date(),
+        status: (req.body.status !== null)
+    });
+    customer.save(function (err) {
+        if (!err) {
+            res.redirect('/');
+        }
     });
 });
 
